cc707ef833d22c5923bc478bbe499e80399a96d1
Kseniia <kseniyau99566@gmail.com>
2024-08-21 15:01:06 +0400

    index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ console.log('Welcome to the Brain Games!');
     
     if (correctAnswer.toString() !== userAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
-      console.log(`Let's try again, ${name}`);
+      console.log(`Let's try again, ${name}!`);
       return;
     }
     console.log('Correct!');
